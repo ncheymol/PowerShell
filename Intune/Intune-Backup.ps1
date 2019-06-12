3186994867ae0fdb18e0a14186587ab4f7a792a1
ncheymol <ncheymol@nelite.com>
2019-06-12 10:31:55 +0200

    Update Intune-Backup.ps1

--- Intune/Intune-Backup.ps1
+++ Intune/Intune-Backup.ps1
@@ -1,7 +1,7 @@
 # Variables
 #$BackupsFoler = "D:\temp\IntuneBackup"
 $logdate = get-date -format "dd-MM-yyyy"
-
+Start-Transcript -path "$env:TEMP\IntuneBackup_$logdate.log"
 ### BackupsFoler
 if ($null -eq $ReferenceFilePath)
 {
@@ -22,7 +22,7 @@ if ($null -eq $credential)
         $Credential = Get-Credential
     }
 
-# test Admin Rights to install Module
+# function test Admin Rights to install Module
 Function Restart-ScriptAsAdmin
 {
     function Test-Admin 
@@ -70,32 +70,215 @@ Function Restart-ScriptAsAdmin
 }
 
 
-# Install Modules
-if ($null -eq (Get-InstalledModule -Name MSGraphFunctions))
-    {
-        Restart-ScriptAsAdmin
-        Install-Module -Name MSGraphFunctions
+# function Get Admin consent to connect to Intune Powershell
+function Get-AuthToken {
+
+    <#
+    .SYNOPSIS
+    This function is used to authenticate with the Graph API REST interface
+    .DESCRIPTION
+    The function authenticate with the Graph API Interface with the tenant name
+    .EXAMPLE
+    Get-AuthToken
+    Authenticates you with the Graph API interface
+    .NOTES
+    NAME: Get-AuthToken
+    #>
+    
+    [cmdletbinding()]
+    
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        $User
+    )
+    
+    $userUpn = New-Object "System.Net.Mail.MailAddress" -ArgumentList $User
+    
+    $tenant = $userUpn.Host
+    
+    Write-Host "Checking for AzureAD module..."
+    
+        $AadModule = Get-Module -Name "AzureAD" -ListAvailable
+    
+        if ($AadModule -eq $null) {
+    
+            Write-Host "AzureAD PowerShell module not found, looking for AzureADPreview"
+            $AadModule = Get-Module -Name "AzureADPreview" -ListAvailable
+    
+        }
+    
+        if ($AadModule -eq $null) {
+            write-host
+            write-host "AzureAD Powershell module not installed..." -f Red
+            write-host "Install by running 'Install-Module AzureAD' or 'Install-Module AzureADPreview' from an elevated PowerShell prompt" -f Yellow
+            write-host "Script can't continue..." -f Red
+            write-host
+            exit
+        }
+    
+    # Getting path to ActiveDirectory Assemblies
+    # If the module count is greater than 1 find the latest version
+    
+        if($AadModule.count -gt 1){
+    
+            $Latest_Version = ($AadModule | select version | Sort-Object)[-1]
+    
+            $aadModule = $AadModule | ? { $_.version -eq $Latest_Version.version }
+    
+                # Checking if there are multiple versions of the same module found
+    
+                if($AadModule.count -gt 1){
+    
+                $aadModule = $AadModule | select -Unique
+    
+                }
+    
+            $adal = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
+            $adalforms = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.Platform.dll"
+    
+        }
+    
+        else {
+    
+            $adal = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
+            $adalforms = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.Platform.dll"
+    
+        }
+    
+    [System.Reflection.Assembly]::LoadFrom($adal) | Out-Null
+    
+    [System.Reflection.Assembly]::LoadFrom($adalforms) | Out-Null
+    
+    $clientId = "d1ddf0e4-d672-4dae-b554-9d5bdfd93547"
+    
+    $redirectUri = "urn:ietf:wg:oauth:2.0:oob"
+    
+    $resourceAppIdURI = "https://graph.microsoft.com"
+    
+    $authority = "https://login.microsoftonline.com/$Tenant"
+    
+        try {
+    
+        $authContext = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext" -ArgumentList $authority
+    
+        # https://msdn.microsoft.com/en-us/library/azure/microsoft.identitymodel.clients.activedirectory.promptbehavior.aspx
+        # Change the prompt behaviour to force credentials each time: Auto, Always, Never, RefreshSession
+    
+        $platformParameters = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.PlatformParameters" -ArgumentList "Auto"
+    
+        $userId = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.UserIdentifier" -ArgumentList ($User, "OptionalDisplayableId")
+    
+        $authResult = $authContext.AcquireTokenAsync($resourceAppIdURI,$clientId,$redirectUri,$platformParameters,$userId,"prompt=admin_consent").Result
+    
+            if($authResult.AccessToken){
+    
+            # Creating header for Authorization token
+    
+            $authHeader = @{
+                'Content-Type'='application/json'
+                'Authorization'="Bearer " + $authResult.AccessToken
+                'ExpiresOn'=$authResult.ExpiresOn
+                }
+    
+            return $authHeader
+    
+            }
+    
+            else {
+    
+            Write-Host
+            Write-Host "Authorization Access Token is null, please re-run authentication..." -ForegroundColor Red
+            Write-Host
+            break
+    
+            }
+    
+        }
+    
+        catch {
+    
+        write-host $_.Exception.Message -f Red
+        write-host $_.Exception.ItemName -f Red
+        write-host
+        break
+    
+        }
+    
     }
-if ($null -eq (Get-InstalledModule -Name IntuneBackupAndRestore))
-    {
-        Restart-ScriptAsAdmin
-        Install-Module -Name IntuneBackupAndRestore
-    }
+    
+
+try 
+{
+    # Install Modules
+    if ($null -eq (Get-InstalledModule -Name MSGraphFunctions))
+        {
+            Restart-ScriptAsAdmin
+            Install-Module -Name MSGraphFunctions
+        }
+    if ($null -eq (Get-InstalledModule -Name IntuneBackupAndRestore))
+        {
+            Restart-ScriptAsAdmin
+            Install-Module -Name IntuneBackupAndRestore
+        }
 
 
-# Import Modules
-if ($null -eq (Get-Module -Name MSGraphFunctions))
-    {
-        Import-Module -Name MSGraphFunctions
+    # Import Modules
+    if ($null -eq (Get-Module -Name MSGraphFunctions))
+        {
+            Import-Module -Name MSGraphFunctions
+        }
+    if ($null -eq (Get-Module -Name IntuneBackupAndRestore))
+        {
+            Import-Module -Name IntuneBackupAndRestore
+        }
     }
-if ($null -eq (Get-Module -Name IntuneBackupAndRestore))
+catch 
     {
-        Import-Module -Name IntuneBackupAndRestore
+    Write-Host "Failed to Import Modules" -ForegroundColor Red        
     }
-
 
 # Connect to Microsoft Graph
-Connect-Graph -Credential $Credential
+try 
+    {
+    Connect-Graph -Credential $Credential -ErrorAction Stop
+    }
+catch 
+    {
+        Write-Host "catch"
+    try 
+        {
+        if ($null -eq (Get-InstalledModule -Name AzureAD))
+            {
+            Restart-ScriptAsAdmin
+            Install-Module -Name AzureAD
+            }
+        if ($null -eq (Get-Module -Name AzureAD))
+            {
+            Import-Module -Name AzureAD
+            }
 
+        }
+    catch 
+        {
+        Write-Host "failed to import AzureAD Module"
+        }
+    try 
+        {
+        Get-AuthToken -User $credential.UserName -ErrorAction Stop 
+        }
+    catch
+        {    
+            Write-host "failed to get rigths"
+        }
+    }
 # Backup Intune
-Start-IntuneBackup -Path $BackupPath
+try 
+    {
+    Start-IntuneBackup -Path $BackupPath    }
+catch 
+    {
+    Write-Host "Failed to Fully backup Intune" -ForegroundColor Red        
+    }
+
+Stop-Transcript
