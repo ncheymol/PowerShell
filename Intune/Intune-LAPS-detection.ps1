dd899f9489a982a5b73c5250eea3998f70e03ff0
ncheymol <49396250+ncheymol@users.noreply.github.com>
2020-06-30 10:30:29 +0200

    Add files via upload

---
+++ Intune/Intune-LAPS-detection.ps1
@@ -0,0 +1,20 @@
+﻿$username = "LocalAdmin"
+$user = Get-LocalUser -Name $username -ErrorAction SilentlyContinue
+if ($user -eq $null)
+    {
+    Write-Host "Not existing"
+    Exit 668
+    }
+Else
+    {
+    $PasswordLastSet = $user.PasswordLastSet
+    if (((Get-Date).AddDays(90) - $PasswordLastSet ) -le 0 )
+        {
+        Write-Host "Expired"
+        Exit 397
+        }
+    Else
+        {
+        Write-Host "OK"
+        }
+    }
