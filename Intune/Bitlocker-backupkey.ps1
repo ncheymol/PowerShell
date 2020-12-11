327dc2e7762c42da0b3c725a79dbae7b34fa32ba
ncheymol <49396250+ncheymol@users.noreply.github.com>
2020-12-11 13:43:04 +0100

    Add files via upload

---
+++ Intune/Bitlocker-backupkey.ps1
@@ -0,0 +1,7 @@
+﻿$letter = "C"
+$Volume = Get-BitLockerVolume -MountPoint $letter
+$KeyProtector = $Volume.KeyProtector | where KeyProtectorType -EQ RecoveryPassword
+$KeyProtectorId = $KeyProtector.KeyProtectorId
+
+Backup-BitLockerKeyProtector -KeyProtectorId $KeyProtectorId -MountPoint $letter
+BackupToAAD-BitLockerKeyProtector -KeyProtectorId $KeyProtectorId -MountPoint $letter
