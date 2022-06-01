0ff84d1c9f026113a435f59e5af36b11ef1d2922
Nicolas CHEYMOL <49396250+ncheymol@users.noreply.github.com>
2022-06-01 17:07:44 +0200

    Add files via upload

---
+++ Intune/Intune-ForceWindowsUpdate.ps1
@@ -0,0 +1,17 @@
+﻿Start-Transcript -Path "c:\ProgramData\Microsoft\IntuneManagementExtension\Logs\force-windowsupdate.log"
+if ((Get-PackageProvider | where Name -EQ Nuget) -eq $null)
+    {
+    Install-PackageProvider -Name NuGet -Force -verbose
+    timeout 10
+    }
+
+if ((Get-Module -Name PSWindowsUpdate) -eq $null)
+    {
+    Install-Module -Name PSWindowsUpdate -Force -verbose
+    timeout 10
+    }
+
+Import-Module PSWindowsUpdate
+Get-WindowsUpdate -Install -AcceptAll -IgnoreReboot -verbose
+Write-host $LASTEXITCODE
+stop-transcript
